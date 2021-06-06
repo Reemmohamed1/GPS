33aaccc98128222e615b22736583ba2869fb6307
A357-a <67614157+A357-a@users.noreply.github.com>
2021-06-06 15:08:03 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 
 void readGPSModule();
-  double latitude = 0, longitude = 0;
-  double distance_calc (void);
+double latitude = 0, longitude = 0;
+double distance_calc (void);
 void led_on (double s);
 
 
 int main(void)
  {
-	   double dist = 0;
-	    portf_init();
-	    LCD_Init();
-       LCD_command(1);                           // clear LCD
-       LCD_command(0x80);                        // Set cursor location to write in first line 
-       LCD_DATA('G');
+	    double dist = 0;
+	     portf_init();
+	     LCD_Init();
+             LCD_command(1);                           // clear LCD
+             LCD_command(0x80);                        // Set cursor location to write in first line 
+             LCD_DATA('G');
 	     LCD_DATA('P');
 	     LCD_DATA('S');
 	     LCD_DATA(' ');
@@ -28,9 +28,9 @@ int main(void)
 	     LCD_DATA('S');
 	     LCD_DATA('Y');
 	     LCD_DATA('S');	
-       			 LCD_command(0xC0);                       // Set cursor location to write in second line
+       	     LCD_command(0xC0);                       // Set cursor location to write in second line
 	     LCD_DATA(' ');
-       LCD_DATA('C');
+             LCD_DATA('C');
 	     LCD_DATA('S');
 	     LCD_DATA('E');                           // Display 'CSE211'
 	     LCD_DATA('2');
@@ -38,17 +38,17 @@ int main(void)
 	     LCD_DATA('1');
 	     LCD_DATA('-');                                 
 	     LCD_DATA('-');
-			 LCD_DATA('T');
+	     LCD_DATA('T');
 	     LCD_DATA('E');	 
 	     LCD_DATA('A');                             // Display 'TEAM62'
 	     LCD_DATA('M');
 	     LCD_DATA('6');
 	     LCD_DATA('2');
-       LCD_DATA(' ');	
-			 LCD_command(1);                           // clear LCD
-       LCD_command(0x80);                                       // Set cursor location to write in first line 
+            LCD_DATA(' ');	
+	    LCD_command(1);                           // clear LCD
+            LCD_command(0x80);                                       // Set cursor location to write in first line 
 	do {
-			 LCD_DATA('D');
+	     LCD_DATA('D');
 	     LCD_DATA('I');
 	     LCD_DATA('S');
 	     LCD_DATA('T');
@@ -63,24 +63,27 @@ int main(void)
 	     LCD_DATA(' ');
 	     LCD_DATA(' ');
 	     LCD_DATA(' ');	
-			 LCD_DATA(' ');			 
-			 LCD_command(0xC0);                         // Set cursor location to write in second line
+	     LCD_DATA(' ');			 
+	     LCD_command(0xC0);                         // Set cursor location to write in second line
 	     LCD_DATA(' ');
-       LCD_DATA(' ');
+             LCD_DATA(' ');
 	     LCD_DATA(' ');
 	     LCD_DATA(' ');                             
 	     LCD_DATA(' ');
 	     LCD_DATA('(');
 	     LCD_DATA('I');                                 
 	     LCD_DATA('N');
-			 LCD_DATA(' ');
+	     LCD_DATA(' ');
 	     LCD_DATA('M');	 
 	     LCD_DATA('E');                             // Display '(IN METERS)'
 	     LCD_DATA('T');
 	     LCD_DATA('E');
 	     LCD_DATA('R');
-			 LCD_DATA('S');
-       LCD_DATA(')');	
+	     LCD_DATA('S');
+             LCD_DATA(')');	
+	   
+		
+	     dist = distance_calc();
 	       
     } while ((GPIO_PORTF_DATA_R &= 0x01) != 1);
 
