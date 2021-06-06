5500887822b091ed73296929897212742fe310e3
nour176yj <54484231+nour176yj@users.noreply.github.com>
2021-06-06 16:57:44 +0200

    Update LCD.c
    Defining a Delay Function to Solve the error of LCD Data not showing on the LCD Display and LCD not Responding to LCD Commands Function.

--- LCD.c
+++ LCD.c
@@ -8,20 +8,39 @@
 #define RW 0x40                                 // PA6 Mask
 #define EN 0x80                                 // PA7 Mask 
 
+void delay_Ms(uint32_t n)
+{
+int i, j;
+for(i = 0 ; i < n; i++)
+for(j = 0; j < 3180; j++)
+{} 
+}
+void delay_Us(uint32_t n)
+{
+int i, j;
+for(i = 0 ; i < n; i++)
+for(j = 0; j < 3; j++)
+{} 
+}
 
 void LCD_command(unsigned char comnd)
  {
    GPIO_PORTA_DATA_R = 0x00;                    // RS = 0 , R/W = 0 
-	 GPIO_PORTB_DATA_R = comnd;                   // Whatever Command is assigned set PORTB Pins to it
-	 GPIO_PORTA_DATA_R |= EN;                     // High Pulse on E    
+   GPIO_PORTB_DATA_R = comnd;                   // Whatever Command is assigned set PORTB Pins to it
+   GPIO_PORTA_DATA_R |= EN;                     // High Pulse on E    
+    delay_Us(0);
    GPIO_PORTA_DATA_R = 0x00;                    // Low Pulse on E 
-    
+    if (comnd < 4)
+        delay_Ms(2); 
+    else
+        delay_Us(40); 
  }
 void LCD_DATA(unsigned char data)
  {
    GPIO_PORTA_DATA_R = RS;                       // RS = 1 , R/W = 0 = 0x20
- 	 GPIO_PORTB_DATA_R = data;                     // Whatever Data is written set PORTB Pins to it
-	 GPIO_PORTA_DATA_R |= EN;                      // High Pulse on E    
+   GPIO_PORTB_DATA_R = data;                     // Whatever Data is written set PORTB Pins to it
+   GPIO_PORTA_DATA_R |= EN;                      // High Pulse on E    
+    delay_Us(0);
    GPIO_PORTA_DATA_R = 0x00;                     // Low Pulse on E 
-   
+    delay_Us(40);
  }
