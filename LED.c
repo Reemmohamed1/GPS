c63f6b27c96edbddc72bd60cb5a538262fa6366a
Ahmed Ashraf <67614157+A357-a@users.noreply.github.com>
2021-06-13 23:35:57 +0200

    Update LED.c

--- LED.c
+++ LED.c
@@ -4,7 +4,7 @@
 #include "tm4c123gh6pm.h"
 #include <stdint.h>
 
-void led_on (double distance)                          // RED LED ON When Distance Exceeds 100 m 
+void led_on (int distance)                          // RED LED ON When Distance Exceeds 100 m 
  {
    if ( distance >= 100)
    GPIO_PORTF_DATA_R |= 0x02;
