153ea7d6a284c7d08555389f1d8b086ff4efd452
AbdullahAli1234 <85312892+AbdullahAli1234@users.noreply.github.com>
2021-06-13 23:22:52 +0200

    Update GPS_parsing.c

--- GPS_parsing.c
+++ GPS_parsing.c
@@ -2,50 +2,9 @@
 #include "stdint.h"
 #include "stdint.h"
 #include "stdlib.h"
-  
-void SystemInit() {}
+ 
 
-void UART0_Init(void){
-    SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R0;
-    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;
-    
-    UART0_CTL_R &= ~UART_CTL_UARTEN;
-    //set buad rate devider
-    UART0_IBRD_R = 104;
-    UART0_FBRD_R = 11;
-		UART0_LCRH_R = (UART_LCRH_WLEN_8 | UART_LCRH_FEN);
-    UART0_CTL_R |= (UART_CTL_UARTEN | UART_CTL_RXE | UART_CTL_TXE);
-
-    GPIO_PORTA_AFSEL_R |= 0x03;
-    GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & ~0xFF) | (GPIO_PCTL_PA0_U0RX | GPIO_PCTL_PA1_U0TX);
-    GPIO_PORTA_DEN_R |= 0x03;
-}
-
-
-void UART0_write(int c){
-    while((UART0_FR_R & UART_FR_TXFF) != 0);
-    UART0_DR_R = c;
-}
-
-void UART5_Init(void){
-SYSCTL_RCGCUART_R |= 0x20; //Activate UART5
-SYSCTL_RCGCGPIO_R |= 0x10; //Activate port E
-UART5_CTL_R = 0x0; //Disable UART5
-UART5_IBRD_R = 104; //IBRD = int(16000000/(16*9600))=int(104.16666)
-UART5_FBRD_R = 11; //FBER = round(0.16666*0.64)
-UART5_LCRH_R = 0x70; //8 bit length ,enablefifo,1-stop bit 
-UART5_CTL_R = 0x301; //enable control registers , RXE,TXE
-GPIO_PORTE_AFSEL_R =0x30; //use ports E4 and E5 alternate functions
-GPIO_PORTE_PCTL_R = 0x00110000; //configure ports E4 and E5 for uart
-GPIO_PORTE_DEN_R = 0x30; //make ports E4 and E5 as digital
-GPIO_PORTE_AMSEL_R &= ~0x00; // no analog on ports E4 and E5
-}
-
-char UART5_read(void){
-    while((UART5_FR_R & 0x10) == 0x10);
-    return UART5_DR_R & 0xFF;
-}
-int main(){
+void getco(double a, double b){
 	char lat[10]="";          //for storing the latitude as an array of characters.
 	char lon[10]="";          //for storing the longitude as an array of characters.
 	char input[] = "" ;       //for storing the incoming input from the GPS-module as an array of characters.
@@ -139,5 +98,6 @@ int main(){
 	num3=atoi(temp);
 	num4= 1.0*num2 + 0.0001*num3;
 	lonfinal = 1.0*num1+(num4/60.0);
-
+a= latfinal;
+b=lonfinal;
 }
