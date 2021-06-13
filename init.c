5d7ffc3374b13c47a9b415a5d08cc33ebe371b65
nour176yj <54484231+nour176yj@users.noreply.github.com>
2021-06-13 23:05:29 +0200

    Update init.c

--- init.c
+++ init.c
@@ -1,12 +1,32 @@
 #include "tm4c123gh6pm.h"
 #include <stdint.h>
-
-
+#include "stdlib.h"
+#include "ustdlib.h"
 //#define RS 0x20                                 // PA5 Mask 
 //#define RW 0x40                                 // PA6 Mask
 //#define EN 0x80                                 // PA7 Mask 
 
 // Initializing PORTS A,B For LCD Requirements 
+
+void UART5_Init(void){
+SYSCTL_RCGCUART_R |= 0x20; //Activate UART5
+SYSCTL_RCGCGPIO_R |= 0x10; //Activate port E
+
+UART5_CTL_R = 0x0; //Disable UART5
+UART5_IBRD_R = 104; //IBRD = int(16000000/(16*9600))=int(104.16666)
+UART5_FBRD_R = 11; //FBER = round(0.16666*0.64)
+UART5_LCRH_R = 0x70; //8 bit length ,enablefifo,1-stop bit 
+UART5_CTL_R = 0x301; //enable control registers , RXE,TXE
+GPIO_PORTE_AFSEL_R =0x30; //use ports E4 and E5 alternate functions
+GPIO_PORTE_PCTL_R = 0x00110000; //configure ports E4 and E5 for uart
+GPIO_PORTE_DEN_R = 0x30; //make ports E4 and E5 as digital
+GPIO_PORTE_AMSEL_R &= ~0x00; // no analog on ports E4 and E5
+}
+char UART5_read(void){
+    while((UART5_FR_R & 0x10) == 0x10);
+    return UART5_DR_R & 0xFF;
+}
+
 void LCD_Init (void)
  {
    SYSCTL_RCGCGPIO_R |= 0x23;                   // Activate Both PORTS A,B,F 
